bc2abbe12c45199f384277c19dececf74cd747b3
Your Name <you@example.com>
2026-06-15 19:20:48 +0530

    another method

--- reversed_str.py
+++ reversed_str.py
@@ -1,10 +1,9 @@
-# def reversed_string():
-#     return word[::-1]
+def reversed_string():
+    #return word[::-1]
+    for i in range(len(word)):
+        print(word[len(word)-1-i],end="")
 
-# word = "This is a string"
-# print(reversed_string())
+word = "This is a string"
+print(reversed_string())
 
-a = "chandu"
 
-for i in range(len(a)):
-    print(a[len(a)-1-i],end="")
